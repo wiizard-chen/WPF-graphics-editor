587bab136e3c7900d7f7fece92dfff9243319141
wiizard chen <wiizard-chen@foxmail.com>
2016-10-11 21:45:18 +0800

    nothing

---
+++ ToolTray/DTTexts.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace ToolTray
+{
+    public class DTTexts : IDWMouseOperation
+    {
+        public void DWMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DWMouseMove(object sender, MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DWMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
