d491c8feb431458a677594f99db781dc1aa1ba1c
wiizard chen <wiizard-chen@foxmail.com>
2016-10-13 09:30:46 +0800

    add Interface

---
+++ ToolTray/Interface/IAdroner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToolTray
+{
+    public interface IAdroner
+    {
+        void AdronerVisble();
+        void AdronerHidden();
+    }
+}
---
+++ ToolTray/Interface/IDynamicShape.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace ToolTray
+{
+    public interface IDynamicShape
+    {
+        /// <summary>
+        /// 新建一个动态编辑的图形
+        /// </summary>
+        /// <param name="point">起始坐标</param>
+        /// <param name="canvas">父类</param>
+        /// <returns></returns>
+        IDynamicShape GetNewShape(Point point, Canvas canvas);
+        /// <summary>
+        /// 图形变形
+        /// </summary>
+        /// <param name="point">变换位置</param>
+        void GraphicDistortion(Point point);
+        /// <summary>
+        /// 图形定型
+        /// </summary>
+        void GraphicDetermine();
+
+    }
+}
